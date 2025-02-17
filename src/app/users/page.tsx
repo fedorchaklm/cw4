d406b70fce64367a6959f1dc946b0c7fe8ae5768
fedorchaklm <fedorchak.luba@gmail.com>
2025-02-18 00:07:22 +0200

    in progress

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,13 +2,14 @@ import {Metadata} from "next";
 import Menu from "@/components/menu/Menu";
 import {FC} from "react";
 import Search from "@/components/search/Search";
-import IUser from "@/models/IUser";
-import User from "@/components/user/User";
 import Pagination from "@/components/pagination/Pagination";
 import {getMaxPages} from "@/helpers/helpers";
 import {limitOfUsersByPage} from "@/constants/constants";
 import NotFound from "@/components/not-found/NotFound";
 import {userService} from "@/services/user.api.service";
+import UsersList from "@/components/users-list/UsersList";
+
+// export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
     title: 'Users page',
@@ -20,22 +21,23 @@ type UsersPageType = {
 };
 
 const UsersPage: FC<UsersPageType> = async ({searchParams}) => {
+    console.log(await searchParams);
     const sp = await searchParams;
     const page = sp?.page || 1;
     const q = sp?.q || '';
+    console.log(q)
     const users = await userService.getUsersByPage(Number(page), q);
-
+    console.log(users);
     return (
         <div className='flex flex-col'>
             <Menu/>
             <div className='flex flex-col items-center gap-2 py-2 w-full text-xl'>
                 <Search/>
                 {users?.users.length > 0 ?
-                    <div className='flex flex-col items-center gap-2 my-2 w-1/3 text-black'>
-                        <h1 className='text-3xl text-white'>Users:</h1>
-                        {users && users.users.map((user: IUser) => <User key={user.id} user={user}/>)}
+                    <>
+                        <UsersList users={users.users}/>
                         <Pagination maxPages={getMaxPages(users.total, limitOfUsersByPage)}/>
-                    </div>
+                    </>
                     : <NotFound/>}
             </div>
         </div>
--- src/components/users-list/UsersList.tsx
+++ src/components/users-list/UsersList.tsx
@@ -1,27 +1,20 @@
 import {FC} from "react";
-import {userService} from "@/services/user.api.service";
 import IUser from "@/models/IUser";
 import User from "@/components/user/User";
-import Pagination from "@/components/pagination/Pagination";
-import {getMaxPages} from "@/helpers/helpers";
-import {limitOfUsersByPage} from "@/constants/constants";
-import Search from "@/components/search/Search";
 import './UserList.css';
-import NotFound from "../not-found/NotFound";
 
-const UsersList: FC = async () => {
-    const users = await userService.getUsersByPage(1, '');
+type UsersListType = {
+    users: Array<IUser>;
+};
+
+const UsersList: FC<UsersListType> = async ({users}) => {
 
     return (
         <div className='list-wrap'>
-            <Search/>
-            {users ?
-                <div className='flex flex-col items-center gap-2 my-2 w-1/3 text-black'>
-                    <h1 className='text-3xl text-white'>Users:</h1>
-                    {users && users.users.map((user: IUser) => <User key={user.id} user={user}/>)}
-                    <Pagination maxPages={getMaxPages(users.total, limitOfUsersByPage)}/>
-                </div>
-                : <NotFound/>}
+            <div className='flex flex-col items-center gap-2 my-2 w-1/3 text-black'>
+                <h1 className='text-3xl text-white'>Users:</h1>
+                {users.map((user: IUser) => <User key={user.id} user={user}/>)}
+            </div>
         </div>
     );
 };
