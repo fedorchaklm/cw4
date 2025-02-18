6c7069f07a13c2ad0dd2764db23e6dba8caf606a
fedorchaklm <fedorchak.luba@gmail.com>
2025-02-19 00:37:05 +0200

    in progress

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,21 +2,18 @@ import axios from "axios";
 import {getCookie} from "cookies-next";
 import {cookies} from "next/headers";
 import {jwtDecode, JwtPayload} from "jwt-decode";
-// import {authService} from "@/services/auth.api.service";
-// import {authService} from "@/services/auth.api.service";
 
 const baseUrl = 'https://dummyjson.com';
 
 export const axiosInstance = axios.create({
     baseURL: baseUrl,
-    // withCredentials: true,
     headers: {"Content-Type": "application/json"},
 });
 
 axiosInstance.interceptors.request.use(async (request) => {
     if (request.method?.toUpperCase() === "GET") {
-        const accessToken = await getCookie('accesstoken', {cookies});
-        console.log(accessToken);
+        const accessToken = await getCookie('accessToken', {cookies});
+        console.log({accessToken});
 
         if (accessToken) {
             request.headers.Authorization = 'Bearer ' + accessToken;
@@ -32,40 +29,6 @@ export const isTokenExpired = (token: string): boolean => {
     const currentTime: number = Math.floor(Date.now() / 1000);
     return decodedData.exp < currentTime;
 };
-
-// axiosInstance.interceptors.request.use(async (request) => {
-//     if (request.method?.toUpperCase() === "GET") {
-//         const accessToken = await getCookie('accesstoken', {cookies});
-//         if (accessToken) {
-//             if (isTokenExpired(accessToken)) {
-//                 try {
-//                     const newToken = await authService.refreshToken();
-//                     if (newToken) {
-//                         request.headers.Authorization = 'Bearer ' + newToken;
-//                     }
-//                 } catch (e) {
-//                     console.info(e);
-//                 }
-//             }
-//         } else {
-//             request.headers.authorization = 'Bearer ' + accessToken;
-//         }
-//     }
-//     return request;
-// });
-
-
-// axiosInstance.interceptors.response.use(
-//     (response) => {
-//         return response;
-//     },
-//     async (error) => {
-//         if (error.response.message === 'Token Expired!') {
-//             await authService.refreshToken();
-//         }
-//         return Promise.reject(error);
-//     }
-// );
 
 
 
