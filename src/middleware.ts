50980b978b3fe039edfe381609eac752f346d5b0
fedorchaklm <fedorchak.luba@gmail.com>
2025-02-19 22:48:46 +0200

    done

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,77 +4,37 @@ import {getCookie} from "cookies-next";
 import {isTokenExpired} from "@/services/api.service";
 import {authService} from "@/services/auth.api.service";
 
-// const checkIsProtectedRoute = (pathname: string) => {
-//     return !pathname.startsWith('/login');
-// };
-
 const publicRoutes = ['/login', '/'];
-
-// export default async function middleware(request: NextRequest) {
-//     const path = request.nextUrl.pathname;
-//     const isProtectedRoute = checkIsProtectedRoute(path);
-//     const accessToken = await getCookie('accesstoken', {cookies}) as string | undefined;
-//     if (accessToken) {
-//         if (isTokenExpired(accessToken)) {
-//             try {
-//                 await authService.refreshToken();
-//             } catch (e) {
-//                 console.info(e);
-//                 return NextResponse.redirect(new URL('/login', request.nextUrl))
-//             }
-//
-//             return NextResponse.next();
-//         }
-//     }
-//
-//     if (isProtectedRoute && !accessToken || isProtectedRoute && typeof accessToken === 'string' && isTokenExpired(accessToken)) {
-//         return NextResponse.redirect(new URL('/login', request.nextUrl))
-//     }
-//
-//     return NextResponse.next();
-// }
-
 
 export default async function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
-    console.log('>', {path});
 
     if (publicRoutes.includes(path)) {
-        console.log('includes');
         return NextResponse.next();
     }
 
-    console.log('not includes');
-    const accessToken = await getCookie('accesstoken', {cookies}) as string | undefined;
-    const currentUser = await getCookie('currentUser', {cookies}) as string | undefined;
-    console.log(accessToken);
-    console.log({currentUser});
+    const accessToken = await getCookie('accessToken', {cookies}) as string | undefined;
+    const currentUser = await getCookie('currentUser', {cookies});
 
-    if (!currentUser) {
-        console.log('no currentUser or accessToken');
+    if (!currentUser || !accessToken) {
         return NextResponse.redirect(new URL('/login', request.nextUrl));
     }
 
     if (accessToken) {
         if (isTokenExpired(accessToken)) {
-            console.log('token expired');
             try {
                 await authService.refreshToken();
             } catch (e) {
                 console.info(e);
-                console.log('token expired can not refresh');
                 return NextResponse.redirect(new URL('/login', request.nextUrl));
             }
-            console.log('refresh token');
             return NextResponse.next();
         }
         return NextResponse.next();
     }
 }
 
-
-// Routes Middleware should not run on
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.jpg$).*)'],
 }
 
